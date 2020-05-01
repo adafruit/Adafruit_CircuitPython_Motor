b95f37b519c2bdc9c52a6e495b2936ac84e2780f
Keith Elliott <k.w.elliott121@gmail.com>
2020-05-01 14:55:29 -0400

    Adding a method to directly set the servo pulse width.

--- adafruit_motor/servo.py
+++ adafruit_motor/servo.py
@@ -70,6 +70,24 @@ class _BaseServo:  # pylint: disable-msg=too-few-public-methods
             raise ValueError("Must be 0.0 to 1.0")
         duty_cycle = self._min_duty + int(value * self._duty_range)
         self._pwm_out.duty_cycle = duty_cycle
+        
+	@property
+	def pulse_width(self):
+		"""The pulse width sent to the servo in microseconds. Must be in
+		the range ``min_pulse`` to ``max_pulse``. Is None when servo is disabled.
+		"""
+		if self._pwm_out.duty_cycle == 0: # Special case for disabled servos
+			return None
+		return self._pwm_out.duty_cycle
+	
+	@pulse_width.setter
+	def pulse_width(self, value):
+		if value is None:
+			self._pwm_out.duty_cycle = 0 # disable the motor
+			return
+		if not self._min_duty <= value <= (self._min_duty + self._duty_range):
+			raise ValueError("Duty cycle out of range")
+		self._pwm_out.duty_cycle = value
 
 
 class Servo(_BaseServo):
