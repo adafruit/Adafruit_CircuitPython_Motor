deff1d61ca5e1627ff08146be847c101b605bef3
Kattni Rembor <kattni@adafruit.com>
2020-08-28 16:38:17 -0400

    Add microcontroller pins to example.

--- examples/motor_stepper_digitalio.py
+++ examples/motor_stepper_digitalio.py
@@ -1,5 +1,5 @@
 # Use this example for digital pin control of an H-bridge driver
-# like a TB6612 or L298N.
+# like a DRV8833, TB6612 or L298N.
 
 import time
 import board
@@ -9,12 +9,25 @@ from adafruit_motor import stepper
 DELAY = 0.01
 STEPS = 200
 
+# You can use any available GPIO pin on both a microcontroller and a Raspberry Pi.
+# The following pins are simply a suggestion. If you use different pins, update
+# the following code to use your chosen pins.
+
+# To use with CircuitPython and a microcontroller:
 coils = (
-    digitalio.DigitalInOut(board.D19),  # A1
-    digitalio.DigitalInOut(board.D26),  # A2
-    digitalio.DigitalInOut(board.D20),  # B1
-    digitalio.DigitalInOut(board.D21),  # B2
+    digitalio.DigitalInOut(board.D9),  # A1
+    digitalio.DigitalInOut(board.D10),  # A2
+    digitalio.DigitalInOut(board.D11),  # B1
+    digitalio.DigitalInOut(board.D12),  # B2
 )
+
+# To use with a Raspberry Pi:
+# coils = (
+#     digitalio.DigitalInOut(board.D19),  # A1
+#     digitalio.DigitalInOut(board.D26),  # A2
+#     digitalio.DigitalInOut(board.D20),  # B1
+#     digitalio.DigitalInOut(board.D21),  # B2
+# )
 
 for coil in coils:
     coil.direction = digitalio.Direction.OUTPUT
