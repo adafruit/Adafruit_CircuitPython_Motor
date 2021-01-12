18db423980389151149e3edee014b683b32972b4
dherrada <dylan.herrada@adafruit.com>
2021-01-12 11:18:59 -0500

    Ran pre-commit, added licenses

--- adafruit_motor/stepper.py
+++ adafruit_motor/stepper.py
@@ -1,24 +1,7 @@
-# The MIT License (MIT)
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft  for Adafruit Industries
 #
-# Copyright (c) 2017 Scott Shawcroft for Adafruit Industries LLC
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# SPDX-License-Identifier: MIT
+
 """
 `adafruit_motor.stepper`
 ====================================================
@@ -174,14 +157,14 @@ class StepperMotor:
         self, *, direction=FORWARD, style=SINGLE
     ):  # pylint: disable=too-many-branches
         """Performs one step of a particular style. The actual rotation amount will vary by style.
-           `SINGLE` and `DOUBLE` will normal cause a full step rotation. `INTERLEAVE` will normally
-           do a half step rotation. `MICROSTEP` will perform the smallest configured step.
+        `SINGLE` and `DOUBLE` will normal cause a full step rotation. `INTERLEAVE` will normally
+        do a half step rotation. `MICROSTEP` will perform the smallest configured step.
 
-           When step styles are mixed, subsequent `SINGLE`, `DOUBLE` or `INTERLEAVE` steps may be
-           less than normal in order to align to the desired style's pattern.
+        When step styles are mixed, subsequent `SINGLE`, `DOUBLE` or `INTERLEAVE` steps may be
+        less than normal in order to align to the desired style's pattern.
 
-           :param int direction: Either `FORWARD` or `BACKWARD`
-           :param int style: `SINGLE`, `DOUBLE`, `INTERLEAVE`"""
+        :param int direction: Either `FORWARD` or `BACKWARD`
+        :param int style: `SINGLE`, `DOUBLE`, `INTERLEAVE`"""
         if self._microsteps is None:
             #
             # Digital IO Pins
