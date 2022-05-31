58802badee803f9052e0a78a4fbc7a4acc1e7519
Alec Delaney <tekktrik@gmail.com>
2022-05-30 21:05:47 -0400

    Add type for actuation_range

--- adafruit_motor/servo.py
+++ adafruit_motor/servo.py
@@ -113,7 +113,10 @@ class Servo(_BaseServo):
     ) -> None:
         super().__init__(pwm_out, min_pulse=min_pulse, max_pulse=max_pulse)
         self.actuation_range = actuation_range
-        """The physical range of motion of the servo in degrees."""
+        """The physical range of motion of the servo in degrees.
+        
+        :type: float
+        """
         self._pwm = pwm_out
 
     @property
